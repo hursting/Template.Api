7d57e2e31045185a5f5a0b130e71152c8d0c5e71
Ciaran Hurst <hursting@gmail.com>
2021-05-23 14:59:01 +0100

    Adding build method to create the FastApi object and set the parameters

---
+++ app/core/config.py
@@ -0,0 +1,5 @@
+VERSION = "0.0.0"
+DEBUG = True
+PROJECT_NAME = "template.api"
+DESCRIPTION = "Reads and Writes Templates to backing data store"
+PROJECT_PREFIX = "/api"
--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from app.core.config import DEBUG, VERSION, DESCRIPTION, PROJECT_NAME
 
-app = FastAPI()
+
+def build_application() -> FastAPI:
+    application = FastAPI(title=PROJECT_NAME, debug=DEBUG, description=DESCRIPTION, version=VERSION)
+    return application
 
 
-@app.get("/")
+template_api = build_application()
+
+
+@template_api.get("/")
 async def root():
     return {"message": "Hello World"}
